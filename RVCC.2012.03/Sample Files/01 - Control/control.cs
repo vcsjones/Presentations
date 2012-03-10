96e7098aaf3af58143cc66dba790e49435e65df4
Kevin Jones <vcsjones@gmail.com>
2012-03-10 11:05:51 -0500

    Blanking out source files. To be filled in later.

--- RVCC.2012.03/Sample Files/01 - Control/control.cs
+++ RVCC.2012.03/Sample Files/01 - Control/control.cs
@@ -1,44 +1,6 @@
-public class Person
-{
-	public string FirstName;
-	public string MiddleName;
-	public string LastName;
-	public int Age;
-	
-	public Person(string firstName, string lastName, string middleName, int age)
-	{
-		FirstName = firstName;
-		LastName = lastName;
-		MiddleName = middleName;
-		Age = age;
-	}
-	
-	public void PrintFullName()
-	{
-		System.Console.WriteLine(FirstName + " " + MiddleName + " " + LastName);
-	}
-}
-
 public class EntryPoint
 {
 	static void Main()
 	{
-		Person[] persons = GetPersons();
-		System.Console.WriteLine("Old People:");
-		foreach(Person person in persons)
-		{
-			if (person.Age >= 30)
-			{
-				person.PrintFullName();
-			}
-		}
-	}
-	
-	private static Person[] GetPersons()
-	{
-		Person[] persons = new Person[2];
-		persons[0] = new Person("Kevin", "Jones", "Hammer", 24);
-		persons[1] = new Person("Jonathan", "Cogley", "Michael", 33);
-		return persons;
 	}
 }
