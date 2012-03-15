9dd915d0f041bfa76f7cfaa3b76ced8ef65231b4
Kevin Jones <vcsjones@gmail.com>
2012-03-15 13:23:53 -0400

    Committing final code used in presentation for RVCC.2012.03

--- RVCC.2012.03/Sample Files/00 - Sample Class/person.cs
+++ RVCC.2012.03/Sample Files/00 - Sample Class/person.cs
@@ -1,6 +1,41 @@
-public class EntryPoint
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstApplication
 {
-	static void Main()
+	public class Person
 	{
+		public string FirstName;
+		public string LastName;
+		public int Age;
+		
+		public void SayHello()
+		{
+			Console.WriteLine("Hello, " + FirstName + " " + LastName);
+			Console.WriteLine("You Are " + Age + " Year(s) old"); 
+		}
+	}
+
+	public class EntryPoint {
+		static void Main()
+		{
+			List<Person> persons = new List<Person>();
+			persons.Add(new Person());
+			persons[0].FirstName = "Kevin";
+			persons[0].LastName = "Jones";
+			persons[0].Age = 500;
+			persons.Add(new Person());
+			persons[1].FirstName = "Jonathan";
+			persons[1].LastName = "Smith";
+			persons[1].Age = 35;
+			foreach(Person p in persons)
+			{
+				p.SayHello();
+				if (p.Age > 40)
+				{
+					Console.WriteLine(p.FirstName + ", you are old.");
+				}
+			}
+		}
 	}
 }
--- RVCC.2012.03/Sample Files/01 - Control/control.cs
+++
@@ -1,6 +0,0 @@
-public class EntryPoint
-{
-	static void Main()
-	{
-	}
-}
