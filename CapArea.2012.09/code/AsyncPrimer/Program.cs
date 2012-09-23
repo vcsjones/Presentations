9041af15790314b29510c6bbad8bc07bed08295f
Kevin Jones <kevin@vcsjones.com>
2012-09-23 18:50:22 -0400

    Adding primer for async (more to do)

---
+++ CapArea.2012.09/code/AsyncPrimer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncPrimer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var result1 = Task.Run(() => Foo());
+            Console.WriteLine("1");
+            var result2 = Task.Run(() => Foo());
+            Console.WriteLine("2");
+            Task.WaitAll(result1, result2);
+        }
+
+        public static async Task Foo()
+        {
+            int number = await GetNumber();
+            Console.WriteLine(number + 6);
+        }
+
+        public static async Task<int> GetNumber()
+        {
+            Thread.Sleep(3000);
+            return 4;
+        }
+    }
+}
